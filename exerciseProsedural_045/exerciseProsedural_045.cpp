dba959d1a756ae3c34ee49efb1073f6830313313
adwitafernanda <adwitafernanda4@gmail.com>
2023-03-29 13:48:46 +0700

    Add project files.

---
+++ exerciseProsedural_045/exerciseProsedural_045.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int Kandidat = 20;
+char nama[20][50];
+int nilaiMatematika[20];
+int nilaiBahasaInggris[20];
+
+void inputData() {
+    for (int i = 0; i < Kandidat; i++) {
+        cout << "Masukkan nama kandidat " << i + 1 << ": ";
+        cin.getline(nama[i], 50);
+        cout << "Masukkan nilai matematika: ";
+        cin >> nilaiMatematika[i];
+        cout << "Masukkan nilai bahasa inggris: ";
+        cin >> nilaiBahasaInggris[i];
+        cin.ignore();
+    }
+}
